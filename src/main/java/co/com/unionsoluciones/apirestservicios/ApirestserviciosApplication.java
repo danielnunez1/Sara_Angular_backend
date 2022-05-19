caa890b0a07709b19b0955bf56e6a12c86920c21
Sebastiian94 <sebastian.martinez25@hotmail.com>
2022-05-19 15:57:16 -0500

    Quitar libreria

--- src/main/java/co/com/unionsoluciones/apirestservicios/ApirestserviciosApplication.java
+++ src/main/java/co/com/unionsoluciones/apirestservicios/ApirestserviciosApplication.java
@@ -1,6 +1,5 @@
 package co.com.unionsoluciones.apirestservicios;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
